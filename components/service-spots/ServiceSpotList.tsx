8d533c3fabfdbd5ac2eddf8774b8da4905d5b6fc
boy52hz <tharadon.saenmart@gmail.com>
2023-12-02 17:29:19 +0700

    feat(service-spots): add message when no service spots found

--- components/service-spots/ServiceSpotList.tsx
+++ components/service-spots/ServiceSpotList.tsx
@@ -2,19 +2,18 @@ import { ServiceSpotDto } from '@cp23pl1/winwin-client-sdk'
 import { FlashList } from '@shopify/flash-list'
 import React from 'react'
 import { Dimensions } from 'react-native'
-import { View } from 'react-native-ui-lib'
 import ServiceSpotListItem, {
   ServiceSpotListItemPressHandler
 } from './ServiceSpotListItem'
+import { Text, View } from 'react-native-ui-lib'
 
 type Props = {
-  items?: ServiceSpotDto[]
+  items: ServiceSpotDto[]
   onItemPress?: ServiceSpotListItemPressHandler
 }
 
 function ServiceSpotList({ items, onItemPress }: Props) {
-  if (!items) return null
-  return (
+  return items.length > 0 ? (
     <View
       style={{
         height: Dimensions.get('screen').height
@@ -27,6 +26,10 @@ function ServiceSpotList({ items, onItemPress }: Props) {
         )}
         estimatedItemSize={100}
       />
+    </View>
+  ) : (
+    <View center marginV-20>
+      <Text caption>ไม่พบวินมอเตอร์ไซค์ในบริเวณนี้</Text>
     </View>
   )
 }
