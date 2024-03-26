77a16b758a0f3fe38b1e089133e8ffe653bc3df4
boy52hz <tharadon.saenmart@mail.kmutt.ac.th>
2024-03-26 14:34:10 +0700

    fix: config new socket client path

--- src/libs/socket-client.ts
+++ src/libs/socket-client.ts
@@ -2,7 +2,8 @@ import { Manager } from 'socket.io-client'
 import auth0 from './auth0'
 
 export const socketManager = new Manager(process.env.EXPO_PUBLIC_SOCKET_URL, {
-  autoConnect: false
+  autoConnect: false,
+  path: process.env.EXPO_PUBLIC_SOCKET_PATH
 })
 
 export const auth0AuthCallback = async (cb: (obj: object) => void) => {
